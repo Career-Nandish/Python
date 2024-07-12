8d72ace2112207daf50e6b84b2c9403d68f29f4a
Career-Nandish <career.nandish@gmail.com>
2024-07-12 20:09:00 +1000

    Arguments taken from GoogleDriveClient and not main.py

--- Projects/1_DriveToMovie/GoogleDrive/GoogleDriveClient.py
+++ Projects/1_DriveToMovie/GoogleDrive/GoogleDriveClient.py
@@ -279,44 +279,44 @@ def is_valid_name(fname):
 
     return True
 
+def take_arguements():
+    parser = ArgumentParser(
+        description="List files from folder IDs with given folder name on the Google Drive."
+        )
+    parser.add_argument(
+        "-t",
+        "--token_filename", 
+        type=str, 
+        default="token.json", 
+        help="User token file."
+        )
+    parser.add_argument(
+        "-c",
+        "--creds_filename", 
+        type=str, 
+        default="credentials.json", 
+        help="User credential file(API keys). Must be a .json File."
+        )
+    parser.add_argument(
+        "-f",
+        "--folder_name", 
+        type=str,
+        required=True,
+        help="Name of the Google Drive Folder with desired files."
+        )
+    parser.add_argument(
+        "-e",
+        "--extensions",
+        nargs='+',
+        default=['image/jpeg', 'image/png', 'image/jpg', 'image/heic', 'image/heif'],
+        help="List of MIME types/extensions of the desired files."
+        )
+
+    return parser.parse_args()
 
 def main():
 
-    parser = ArgumentParser(
-                description="List files from folder IDs with given folder name on \
-                the Google Drive."
-             )
-    parser.add_argument(
-               "-t",
-               "--token_filename", 
-               type=str, 
-               default="token.json", 
-               help="User token file."
-           )
-    parser.add_argument(
-               "-c",
-               "--creds_filename", 
-               type=str, 
-               default="credentials.json", 
-               help="User credential file(API keys). Must be a .json File."
-           )
-    parser.add_argument(
-               "-f",
-               "--folder_name", 
-               type=str,
-               required=True,
-               help="Name of the Google Drive Folder with desired files."
-           )
-    parser.add_argument(
-               "-e",
-               "--extensions",
-               nargs='+',
-               default=['image/jpeg', 'image/png', 'image/jpg', 'image/heic', 'image/heif'],
-               help="List of MIME types/extensions of the desired files."
-    )
-
-    args = parser.parse_args()
-    
+    args = take_arguements()
     if is_valid_name(args.folder_name): 
         googledriveclient = GoogleDriveClient(args.token_filename, args.creds_filename)
         folder_ids = googledriveclient.get_folder_id(args.folder_name)
--- Projects/1_DriveToMovie/GoogleDrive/main.py
+++ Projects/1_DriveToMovie/GoogleDrive/main.py
@@ -1,42 +1,6 @@
-from GoogleDriveClient import GoogleDriveClient, is_valid_name
+from GoogleDriveClient import GoogleDriveClient, is_valid_name, take_arguements
 from argparse import ArgumentParser
 from FilesCreatedTime import get_actual_createdTime
-
-
-def take_arguements():
-	parser = ArgumentParser(
-		description="List files from folder IDs with given folder name on the Google Drive."
-		)
-	parser.add_argument(
-		"-t",
-		"--token_filename", 
-		type=str, 
-		default="token.json", 
-		help="User token file."
-		)
-	parser.add_argument(
-		"-c",
-		"--creds_filename", 
-		type=str, 
-		default="credentials.json", 
-		help="User credential file(API keys). Must be a .json File."
-		)
-	parser.add_argument(
-    	"-f",
-    	"--folder_name", 
-    	type=str,
-    	required=True,
-    	help="Name of the Google Drive Folder with desired files."
-    	)
-	parser.add_argument(
-    	"-e",
-    	"--extensions",
-    	nargs='+',
-    	default=['image/jpeg', 'image/png', 'image/jpg', 'image/heic', 'image/heif'],
-    	help="List of MIME types/extensions of the desired files."
-    	)
-
-	return parser.parse_args()
 
 
 def main():
