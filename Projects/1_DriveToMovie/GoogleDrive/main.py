c8d09c7625e8ef9041dfcec28fa2ecefcf367272
Career-Nandish <career.nandish@gmail.com>
2024-07-16 04:39:18 +1000

    cowsay include

--- Projects/1_DriveToMovie/GoogleDrive/main.py
+++ Projects/1_DriveToMovie/GoogleDrive/main.py
@@ -1,5 +1,6 @@
 import sys
 from os import path
+from cowsay import get_output_string
 sys.path.append(path.abspath("google_drive"))
 from GoogleDriveClient import GoogleDriveClient, is_valid_name, take_arguements
 from argparse import ArgumentParser
@@ -15,18 +16,19 @@ from Audiofy import audiofy
 
 
 def main():
+	
 	args = take_arguements()
 	if is_valid_name(args.folder_name):
-		# googledriveclient = GoogleDriveClient(args.token_filename, args.creds_filename)
-		# folder_ids = googledriveclient.get_folder_id(args.folder_name)
-		# files = googledriveclient.get_files_from_folder(folder_ids, args.extensions)
-		# downloading_path = manage_files(args.folder_name, files, googledriveclient.creds)
-		# modified_folder_path = image_modifier(downloading_path)
-		# video_path = video_writer(modified_folder_path, args.duration_video_sec)
-		# final_result_path = audiofy(video_path)
-		final_result_path = "resources\\final\\final.mp4"
+		print(get_output_string('cow', 'Starting the project'))
+		googledriveclient = GoogleDriveClient(args.token_filename, args.creds_filename)
+		folder_ids = googledriveclient.get_folder_id(args.folder_name)
+		files = googledriveclient.get_files_from_folder(folder_ids, args.extensions)
+		downloading_path = manage_files(args.folder_name, files, googledriveclient.creds)
+		modified_folder_path = image_modifier(downloading_path)
+		video_path = video_writer(modified_folder_path, args.duration_video_sec)
+		final_result_path = audiofy(video_path)
 		done_result_path = video_enhancer(final_result_path)
-
+		print(get_output_string('cow', 'Ending the project'))
 	else:
 		print("==== Invalid Folder Name. ====")
 		print("\nPlease ensure the Folder Name follows these rules:")
