60e439aa89fdfb0e7ee63d6c578d0e055e3db961
Career-Nandish <career.nandish@gmail.com>
2024-07-15 10:15:22 +1000

    Downloading files, image alteration flow added

--- Projects/1_DriveToMovie/GoogleDrive/main.py
+++ Projects/1_DriveToMovie/GoogleDrive/main.py
@@ -1,6 +1,7 @@
 from GoogleDriveClient import GoogleDriveClient, is_valid_name, take_arguements
 from argparse import ArgumentParser
 from FilesCreatedTime import get_actual_createdTime
+from ImageHandler import image_modifier
 
 
 def main():
@@ -9,6 +10,9 @@ def main():
 		googledriveclient = GoogleDriveClient(args.token_filename, args.creds_filename)
 		folder_ids = googledriveclient.get_folder_id(args.folder_name)
 		files = googledriveclient.get_files_from_folder(folder_ids, args.extensions)
+		downloading_path = manage_files(folder_name, files, googledriveclient.creds)
+		modified_folder_path = image_modifier(downloading_path)
+
 	else:
 		print("==== Invalid Folder Name. ====")
 		print("\nPlease ensure the Folder Name follows these rules:")
