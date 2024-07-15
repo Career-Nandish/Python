f71c38c11fda01df05c6c103f25ff65edc1b0252
Career-Nandish <career.nandish@gmail.com>
2024-07-15 11:08:23 +1000

    Video Writer flow added to main.py

--- Projects/1_DriveToMovie/GoogleDrive/main.py
+++ Projects/1_DriveToMovie/GoogleDrive/main.py
@@ -1,7 +1,9 @@
 from GoogleDriveClient import GoogleDriveClient, is_valid_name, take_arguements
 from argparse import ArgumentParser
 from FilesCreatedTime import get_actual_createdTime
+from FileDownloader import manage_files
 from ImageHandler import image_modifier
+from VideoWriter import video_writer
 
 
 def main():
@@ -10,8 +12,9 @@ def main():
 		googledriveclient = GoogleDriveClient(args.token_filename, args.creds_filename)
 		folder_ids = googledriveclient.get_folder_id(args.folder_name)
 		files = googledriveclient.get_files_from_folder(folder_ids, args.extensions)
-		downloading_path = manage_files(folder_name, files, googledriveclient.creds)
+		downloading_path = manage_files(args.folder_name, files, googledriveclient.creds)
 		modified_folder_path = image_modifier(downloading_path)
+		video_path = video_writer(modified_folder_path)
 
 	else:
 		print("==== Invalid Folder Name. ====")
