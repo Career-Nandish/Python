6947a3393341d93c6f55c6fc2ee8d19adcf5ba25
Career-Nandish <career.nandish@gmail.com>
2024-07-26 18:06:53 +1000

    CodewarsAutomated initiated

---
+++ Projects/2_CodewarsAutomated/main.py
@@ -0,0 +1,25 @@
+
+
+
+def main():
+	
+	#1 github authentication to see if repo exists
+	
+	#2 if not make one
+
+	# extract data from codewars, 
+	# markdownify and create a file
+	# make commits, with commit number, total challenges
+
+	#3 if yes then check what was the last commit, which would contain 
+	# the total problems
+
+	#4 make it so the script runs every nights at 12
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
