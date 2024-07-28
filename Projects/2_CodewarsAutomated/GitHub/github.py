053a79625b13521bd67c737344b9f79cb95b76ae
Career-Nandish <career.nandish@gmail.com>
2024-07-28 22:35:42 +1000

    Checking markdown exists or not Done

--- Projects/2_CodewarsAutomated/GitHub/github.py
+++ Projects/2_CodewarsAutomated/GitHub/github.py
@@ -1,6 +1,8 @@
 import json
 from github import Github, GithubException, AuthenticatedUser
+from github import ContentFile
 from pathlib import Path
+from typing import Union
 
 
 def load_credentials() -> str:
@@ -86,5 +88,88 @@ def get_github_user(PAT: str) -> AuthenticatedUser:
 
     except Exception as e:
         # Handle other potential exceptions
-        print(f"\n\n****AN UNEXPECTED ERROR OCCURRED ****: {e}\n\n")
+        print(f"\n\n****AN UNEXPECTED ERROR OCCURRED IN get_github_user ****: {e}\n\n")
         raise
+
+
+def check_folder_exists(
+        user: AuthenticatedUser, 
+        repo_name: str = "Python",
+        project_path: str = "Projects/2_CodewarsAutomated",
+        folder_name: str = "Generated",
+        file_name: str = "CodewarsAutomated.md"
+    ) -> Union[ContentFile.ContentFile, str]:
+    
+    """
+    Check if a specific folder and file exist in a GitHub repository.
+
+    Args:
+        user (AuthenticatedUser): The authenticated GitHub user.
+        repo_name (str): The name of the repository. Defaults to "Python".
+        project_path (str): The path of the project within the repository. 
+                            Defaults to "Projects/2_CodewarsAutomated".
+        folder_name (str): The name of the folder to check within the project 
+                           path. Defaults to "Generated".
+        file_name (str): The name of the file to check within the folder. Defaults 
+                         to "CodewarsAutomated.md".
+
+    Returns:
+        Union[ContentFile.ContentFile, str]: The ContentFile if found, otherwise the 
+                                             path of the file as a string.
+    """
+
+    # Try block for error handling
+    try:
+        
+        # Display for user
+        print(f"\n==== Searching for repository : '{repo_name}'. ====")
+        
+        # Check if Repository exits
+        repo = user.get_repo(repo_name)
+        
+        # Display for user
+        print(f"\n==== Repository : '{repo_name}' found. ====")
+
+        # Path desired
+        path_d = f"{project_path}/{folder_name}/{file_name}"
+        
+        print(f"\n==== Searching for '{path_d}'. ====")
+
+        # Check if path exists
+        contents = repo.get_contents(f"""{project_path}/{folder_name}/
+            {file_name}""")
+
+        print(f"\n==== File '{path_d}' has been found. ====")
+
+        return contents
+
+    except GithubException as e:
+
+        # Try to find what type of error it is using url data
+        documentation_url = e.data.get("documentation_url")
+
+        # Check what kind of error we have encountered
+        ## First if the directory doesn't exist
+        if "content" in documentation_url:
+            print(f"""\n\n**** DIRECTORY '{project_path}' DOESN'T EXIST IN THE 
+                REPOSITORY '{repo_name}'. ****\n\n""")
+        
+        ## Second if the repository doesn't exist
+        elif "repository" in documentation_url:
+            print(f"""\n\n**** REPOSITORY '{repo_name}' DOESN'T EXIST UNDER 
+                GITHUB USER '{user.login}'. ****\n\n""")
+
+        else:
+            print(f"\n\n**** AN ERROR OCCURRED: {e.data.get('message', 
+                'Unknown error')} ****\n\n")
+
+    except Exception as e:
+        # Handle other potential exceptions
+        print(f"""\n\n****AN UNEXPECTED ERROR OCCURRED IN check_folder_exists 
+            ****: {e}\n\n""")
+        raise
+    
+    finally:
+        # Finally block
+        print(f"\n==== File '{path_d}' has not been found. Returning Path. ====")
+        return path_d
