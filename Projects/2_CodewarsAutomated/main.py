950d3f3613b08edfc7d1bb4a57e5ffda65cd1ade
Career-Nandish <career.nandish@gmail.com>
2024-07-30 20:57:47 +1000

    flow changed

--- Projects/2_CodewarsAutomated/main.py
+++ Projects/2_CodewarsAutomated/main.py
@@ -4,21 +4,24 @@ from github import Github, GithubIntegration
 from github import GithubException, AuthenticatedUser
 from github import ContentFile
 
-
 def main():    
 	
-	# PAT = gh.load_gh_credentials()
-	# guser = gh.get_github_user(PAT)
+	PAT = gh.load_gh_credentials()
+	guser = gh.get_github_user(PAT)
+	
 	# content = gh.check_folder_exists(guser)
+	# print(content)
 
 	# check if content is contentfile or str
 	# based on that do future things
 
 	# codewars
-	driver = cw.get_webdriver()
 	creds = cw.load_cw_credentials()
-	
-	cw.codewars_navigate(driver, creds)
+
+	# If you have different usernames for github and codewars
+	# please change it here or hardcode it as default value
+	# in the function below
+	cw.codewars_login(creds, username = guser.login)
 
 if __name__ == '__main__':
 	main()
