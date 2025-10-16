e917beeac3ea25209d2b7865567cb2e735cb91dc
Career-Nandish <career.nandish@gmail.com>
2025-10-16 23:35:57 +0530

    minor changes

--- Projects/2_CodewarsAutomated/main.py
+++ Projects/2_CodewarsAutomated/main.py
@@ -6,30 +6,40 @@ from github import ContentFile
 def main():    
 	
 	# Loading credentials
-	PAT = gh.load_gh_credentials()
+	#PAT = gh.load_gh_credentials()
 	
 	# Authenticating the PAT
-	gUser = gh.get_github_user(PAT)
+	#gUser = gh.get_github_user(PAT)
 	
 	# Check if the folder/file already exists or not
-	content = gh.check_folder_exists(gUser)
+	#gContent, repo_path = gh.check_folder_exists(gUser)
 
-	#print(content)
+	#print(gContent, repo_path)
 	
 	# check if content is ContentFile or str
-	if isinstance(content, ContentFile.ContentFile):
-		pass
-	else:
-		init_project(guser, content)
+	#if gContent:
+	#	pass
+	#else:
+	#	init_project(gUser, gContent)
 	# based on that do future things
 
 	# codewars
-	# creds = cw.load_cw_credentials()
+
+	# loading credentials
+	cw_email, cw_password = cw.load_cw_credentials()
+	
+	# Loading session and the CSRF token
+	cw_session, cw_token = cw.start_cw_session()
+
+	cw_session, cw_username = cw.login_codewars(
+		                          cw_session, cw_email, cw_password, cw_token
+		                       )
+
+	print(cw_session, cw_username)
 
 	# If you have different usernames for github and codewars
 	# please change it here or hardcode it as default value
 	# in the function below
-	#cw.codewars_login(creds, username = guser.login)
 
 if __name__ == '__main__':
 	main()
