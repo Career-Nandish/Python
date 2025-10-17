26091ab66f47320c103ed7bd9016b1400cd204f9
Career-Nandish <career.nandish@gmail.com>
2025-10-17 19:19:33 +0530

    minor changes

--- Projects/2_CodewarsAutomated/main.py
+++ Projects/2_CodewarsAutomated/main.py
@@ -4,16 +4,16 @@ from github import ContentFile
 
 
 def main():    
-	
+
 	# First run
 	first_run = True
 
 	# Loading credentials
-	PAT = gh.load_gh_credentials()
+	#gPAT = gh.load_gh_credentials()
 	cw_email, cw_password = cw.load_cw_credentials()
 	
 	# Authenticating the PAT
-	gUser = gh.get_gh_user(PAT)
+	#gUser = gh.get_gh_user(gPAT)
 
 	# Loading session and the CSRF token
 	cw_session, cw_token = cw.start_cw_session()
@@ -23,16 +23,20 @@ def main():
 		                       )
 	
 	# Check if the folder/file already exists or not
-	gContent, repo_path = gh.check_gh_folder_exists(gUser)
+	gContent, gRepo_path = gh.check_gh_folder_exists(gUser)
 
-	print(gContent, repo_path)
+	#print(gContent, repo_path)
 	
+	gContent = None
+
 	# check if content is ContentFile or str
-	# if gContent:
-	# 	first_run = False
-	# else:
-	# 	download_cw_solutions(gContent, first_run, )
-	# 	init_gh_project(gUser, cContent)
+	if gContent:
+		first_run = False
+	else:
+		cContent = cw.download_cw_solutions(
+			           cw_session, cw_username, cw_token, gContent, first_run
+			       )
+		#gh.init_gh_project(gUser, cContent)
 	# based on that do future things
 
 	# codewars
